21a4a4cd1ff7f6c1fd6b22699fbf414230e8a6e0
TJ Stankus <tjstankus@gmail.com>
2023-09-06 15:21:01 -0400

    Rename

--- lib/wordshape_cli/game.rb
+++ lib/wordshape_cli/game.rb
@@ -2,19 +2,19 @@
 
 module WordshapeCli
   class Game
-    attr_reader :solution_chars, :guess_matches
+    attr_reader :solution_chars, :matches
 
     def initialize(solution_word)
       @solution_chars = solution_word.chars
     end
 
     def guess(guess_word)
-      @guess_matches = init_matches(guess_word.chars)
+      @matches = init_matches(guess_word.chars)
 
-      guess_matches.each_with_index do |guess_match, i|
+      matches.each_with_index do |guess_match, i|
         guess_char = guess_match.char
         if guess_char == solution_chars[i]
-          guess_matches[i] = Match.new(char: guess_char, type: :position)
+          matches[i] = Match.new(char: guess_char, type: :position)
         elsif solution_chars.include?(guess_char)
           do_letter_match(guess_char)
         end
@@ -28,14 +28,14 @@ module WordshapeCli
     end
 
     def do_letter_match(guess_char)
-      num_non_nil_guess_matches = guess_matches.count { |m| !m.nil? }
+      num_non_nil_matches = matches.count { |m| !m.nil? }
       num_solution_chars = solution_chars.count(guess_char)
-      set_letter_match(guess_char) if num_non_nil_guess_matches < num_solution_chars
+      set_letter_match(guess_char) if num_non_nil_matches < num_solution_chars
     end
 
     def set_letter_match(char)
-      index = guess_matches.index { |m| m.char == char && m.nil? }
-      guess_matches[index] = Match.new(char:, type: :letter)
+      index = matches.index { |m| m.char == char && m.nil? }
+      matches[index] = Match.new(char:, type: :letter)
     end
   end
 end
